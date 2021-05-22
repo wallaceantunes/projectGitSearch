6443626311217ada10722828b7b5b5c0dd9cd836
Wallace Antunes <wallace.antunes@ubiqui.com.br>
2021-05-22 01:13:33 -0300

    adicionando tela de resultado e ajustando navBar

---
+++ src/components/CardResults.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>opa</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container nav">
+    <div class="row">
+      <div class="col offset-md-2 md-8 offset-lg-2 lg-8">
+        <div class="nav-col" @click="goHome">
+          <img class="img-responsive" src="@/assets/images/gitHubLogo.png" width="40px" height="40px">
+          <span class="text-nav">GitSearch</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goHome () {
+      this.$router.push({ name: 'Search' })
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/navBar.scss'
+</style>
